e2e43764dba21dc8f0310202a73dbcfa35170447
Caroline Owen <carolinaowen95@gmail.com>
2022-10-12 10:50:31 +0200

    Ajoutez des fichiers projet.

---
+++ nombre_magique/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static int demanderNombre(int min, int max) {
+    int nombre = min - 1;
+    while (((nombre < min) || (nombre > max)))
+    {
+        Console.WriteLine($"veuillez choisir un nombre entre {min} et {max}");
+        string reponse = Console.ReadLine();
+
+        try
+        {
+            nombre= int.Parse(reponse);
+            if ((nombre < min)||(nombre > max))
+            {
+                Console.WriteLine($"le nombre doit etre compris entre {min} et {max}");
+            }
+        }
+        catch{
+            Console.WriteLine("veuillez rentrer un nombre valide");
+        }
+
+} 
+    return nombre;
+}
+Random rand = new Random();
+const int MAX = 10;
+const int MIN= 1;
+int MAGIQUE = rand.Next(MIN, MAX+1);
+int nombreAffiche = MAGIQUE+1;
+
+while (nombreAffiche != MAGIQUE)
+{
+    nombreAffiche = demanderNombre(MIN, MAX);
+   if (nombreAffiche > MAGIQUE)
+    {
+        Console.WriteLine("le nombre est plus petit");
+    }
+    else if (nombreAffiche < MAGIQUE)
+    {
+        Console.WriteLine("le nombre est plus grand");
+    }
+}
+
+Console.WriteLine("Bravo");
+
+//Random rand = new Random();
+//int result = rand.Next(1, 10);
+//Console.WriteLine("le nombre aléatoire est " + result);
